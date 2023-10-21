346bebb678840035042583fbd7e36d2cddc1de99
Deepak <deepakdas538@gmail.com>
2023-10-21 19:23:51 +0530

    changed the color theme and animation

--- login.js
+++ login.js
@@ -1,7 +1,4 @@
-const toggleForm = () => {
-    const container = document.querySelector('.container');
-    container.classList.toggle('active');
-  };
+
   var loader = document.getElementById('loading');
   mobile_nav.addEventListener("click", () => toggleNavbar());
 function loader_off(){
