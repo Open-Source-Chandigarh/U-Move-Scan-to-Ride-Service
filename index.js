ac4b923cafdd4c4cdd89872480816eff34cf5368
Luis A.Tavarez <51054204+uppy19d0@users.noreply.github.com>
2023-10-29 03:28:48 -0400

    Update index.js
    In this improved version:
    
    Constants are defined at the beginning of the script, making it easier to manage and understand.
    
    The toggle and remove functions for the navigation are separate, enhancing readability.
    
    Event listeners are clearly defined and follow a logical order.
    
    The loader-off function is renamed to turnOffLoader for better readability.
    
    The URL and form are declared as constants for easier modification if needed.

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
-const mobile_nav = document.querySelector(".mobile-navbar-btn");
-const nav_header = document.querySelector(".header");
-var links=document.querySelectorAll(".header .navbar .navbar-list li")
-var loader = document.getElementById('loading');
-const toggleNavbar = () => {
-  nav_header.classList.toggle("active");
-}
-const removeNavbar = () => {
-    nav_header.classList.remove("active");
-}
+// Define constants and query selectors
+const mobileNavButton = document.querySelector(".mobile-navbar-btn");
+const navHeader = document.querySelector(".header");
+const links = document.querySelectorAll(".header .navbar .navbar-list li");
+const loader = document.getElementById('loading');
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyeqRCyDCUkcQHm78ZHnvDy9vekLsILnhHFcr1JV4wyMaGamAF2TcujryZq8cbh1rgP/exec';
+const form = document.forms['submit-to-google-sheet'];
 
+// Function to toggle the navigation
+const toggleNavbar = () => {
+  navHeader.classList.toggle("active");
+};
+
+// Function to hide the navigation
+const removeNavbar = () => {
+  navHeader.classList.remove("active");
+};
+
+// Event listeners for link clicks
 links.forEach((li) => {
   li.addEventListener("click", removeNavbar);
 });
-mobile_nav.addEventListener("click", () => toggleNavbar());
-function loader_off(){
-    loader.style.display='none';
 
+// Event listener for mobile nav button
+mobileNavButton.addEventListener("click", toggleNavbar);
+
+// Function to turn off the loader
+function turnOffLoader() {
+  loader.style.display = 'none';
 }
 
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbyeqRCyDCUkcQHm78ZHnvDy9vekLsILnhHFcr1JV4wyMaGamAF2TcujryZq8cbh1rgP/exec'
-  const form = document.forms['submit-to-google-sheet']
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response),form.reset())
-      .catch(error => console.error('Error!', error.message))
-  })
+// Event listener for form submission
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+    .then((response) => {
+      console.log('Success!', response);
+      form.reset();
+    })
+    .catch((error) => {
+      console.error('Error!', error.message);
+    });
+});
